d948d3fad5a53b37a050f01e17b2e0963eb576fa
egor2910 <147133922+egor2910@users.noreply.github.com>
2024-03-22 17:58:40 +1000

    Add files via upload

---
+++ bunker-main/js/indexx.js
@@ -0,0 +1,16 @@
+let title = document.querySelector(".title")
+let card = document.querySelector(".person")
+let btnGenerate = document.querySelector("#create")
+
+console.log(title,card,btnGenerate)
+console.dir(title)
+btnGenerate.addEventListener("click",()=>{
+    console.log("terra")
+    title.style.opacity = 0;
+    setTimeout(() => {
+         title.style.display = "none"
+         card.style.opacity = 100
+         card.style.display = flex
+    }, 1000);
+   
+})
