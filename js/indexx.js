52bd7a826e1a0f4889f6aefd7e7431fa5ef33257
egor2910 <147133922+egor2910@users.noreply.github.com>
2024-03-29 18:00:40 +1000

    Add files via upload

--- js/indexx.js
+++ js/indexx.js
@@ -1,16 +1,31 @@
+import { NAME_MASS, PROFESSION_MASS } from "./massives/module.js";
+
+
 let title = document.querySelector(".title")
 let card = document.querySelector(".person")
 let btnGenerate = document.querySelector("#create")
+let personItemMass = document.querySelectorAll(
+    ".person-item")
 
-console.log(title,card,btnGenerate)
-console.dir(title)
+
+ function random(min,max) {
+    return Math.floor (Math.random()*(max-min)+min)
+ }
+
+
 btnGenerate.addEventListener("click",()=>{
     console.log("terra")
     title.style.opacity = 0;
+    personItemMass[0].innerText = "Имя:"+ NAME_MASS[random(0,NAME_MASS.length)]
+    personItemMass[2].innerText = "Профессия:"+ PROFESSION_MASS[random(0,PROFESSION_MASS.length)]
     setTimeout(() => {
          title.style.display = "none"
-         card.style.opacity = 100
-         card.style.display = flex
-    }, 1000);
-   
+          card.style.display = "flex"
+          setTimeout (() =>{
+
+              card.style.opacity = 100
+            },1000)
+    }, 1000);   
 })
+console.log(personItemMass)
+console.log(personItemMass [3])
---
+++ js/massives/module.js
@@ -0,0 +1,7 @@
+const PROFESSION_MASS = ["Вахтер","Уборщик", "Инжинер", "Врач",
+ "Убийца",]
+ const NAME_MASS = ["Вова","Миша", "Тима",
+ "Никита"]
+
+
+ export { NAME_MASS, PROFESSION_MASS }
