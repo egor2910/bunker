512f349cdcd2555c1c581014b12885d0d1af9173
egor2910 <147133922+egor2910@users.noreply.github.com>
2024-04-03 07:24:05 -0400

    Add files via upload

--- js/indexx.js
+++ js/indexx.js
@@ -1,4 +1,12 @@
-import { NAME_MASS, PROFESSION_MASS } from "./massives/module.js";
+// import { NAME_MASS, PROFESSION_MASS, HEALTH_MASS,FEAR_MASS, HOBBY_MASS } from "./massives/module.js";
+const PROFESSION_MASS = ["Вахтер","Уборщик", "Инжинер", "Врач",
+ "Убийца",]
+ const NAME_MASS = ["Вова","Миша", "Тима",
+ "Никита"]
+ const HEALTH_MASS = ["туберкулез", "здоров как бык", "умирает", "нет правой руки", "деменция",
+  "синдром Торета"]
+ const FEAR_MASS = ["пауки", "тараканы", "высота", "темнота", "крысы", "клоуны", "замкнутые пространства"]
+const HOBBY_MASS = ["чтение книг", "шахматист", "игрок", "боксер", "писатель", "ботаник", "грибник"]
 
 
 let title = document.querySelector(".title")
@@ -17,7 +25,14 @@ btnGenerate.addEventListener("click",()=>{
     console.log("terra")
     title.style.opacity = 0;
     personItemMass[0].innerText = "Имя:"+ NAME_MASS[random(0,NAME_MASS.length)]
+    personItemMass[1].innerText = "Возраст:"+ [random(15,55)]
     personItemMass[2].innerText = "Профессия:"+ PROFESSION_MASS[random(0,PROFESSION_MASS.length)]
+    personItemMass[3].innerText = "Стаж работы:"+ [random(0,15)]
+    personItemMass[4].innerText = "Здоровье:"+ HEALTH_MASS[random(0,HEALTH_MASS.length)]
+    personItemMass[5].innerText = "Плодовитость:"+ [random(0,10)]
+    personItemMass[6].innerText = "Страхи:"+ FEAR_MASS[random(0,FEAR_MASS.length)]
+    personItemMass[7].innerText = "Хобби:"+ HOBBY_MASS[random(0,HOBBY_MASS.length)]
+
     setTimeout(() => {
          title.style.display = "none"
           card.style.display = "flex"
